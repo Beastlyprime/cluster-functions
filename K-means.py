17cb5e2c3beb9ed363b6c601c1295eb06619b2fb
Beastlyprime <452757387@qq.com>
2017-06-27 00:21:39 +0800

    Finish function

--- K-means.py
+++ K-means.py
@@ -1,22 +1,51 @@
 import numpy as np
-import pandas as pd
+from collections import Counter
 
-filename = "/Users/chenyimeng/Documents/courses/Data mining/hw3/Iris_train.csv"
+filename = "/Users/chenyimeng/Documents/courses/Data mining/hw3/Iris_train.txt"
 
-data = pd.read_csv(filename)
-label = np.array(data['label'])
+changes = 10
 
-
-class kmeans_center(object):
-    def __init__(self, coor, label):
+class Point(object):
+    def __init__(self, coor, label, id=-1):
         self.coor = coor
         self.label = label
+        self.id = id
 
-class K_centers(object):
-    def __init__(self,x, center_num=5):
+# a cluster
+class kmeans_center(object):
+    def __init__(self, coor, id):
+        # the coordinate of its center
+        self.coor = coor
+        # the coordinates of all the pts in it
+        self.members = []
+        # the label of all the pts in it
+        self.label = []
+        # the number of pts in it
+        self.num = 0.
+        self.id = id
+
+    # get a new point into this cluster
+    def get_in(self, new_point):
+        global changes
+        self.members.append(new_point.coor)
+        self.label.append(new_point.label)
+        self.num += 1
+        if new_point.id != self.id:
+            new_point.id = self.id
+            changes += 1
+        return new_point
+
+    def get_label(self):
+        return Counter(self.label).most_common(1)[0][0]
+
+# k clusters
+class K_clusters(object):
+    def __init__(self, x, center_num=5):
         self.nparray = x
         self.center_num = center_num
-        self.centers = []
+        self.centers = [] # the list of kmeans_center. id is responding to that.
+
+    # choose k centers randomly
     def initial(self):
         nsample, nfeature = self.nparray.shape
         center_id = []
@@ -25,25 +54,77 @@ class K_centers(object):
             while new_id in center_id:
                 new_id = np.random.randint(0, nsample)
             center_id.append(new_id)
-            new_center = kmeans_center(self.nparray[new_id,0:4],self.nparray[new_id,4])
-            print new_center.coor
+            new_center = kmeans_center(self.nparray[new_id,0:4], i)
             self.centers.append(new_center)
 
+    def restart(self):
+        for center in self.centers:
+            center.coor = np.array(center.members).sum(0) / len(center.members)
+            center.num = 0.
+            center.label = []
+            center.members = []
+
+# return the id of the nearest cluster to a point
 def min_distance(centers, cand_point):
-    dist = np.square(centers-cand_point)
+    coors = []
+    for center in centers.centers:
+        coors.append(center.coor)
+    dist = np.square(coors-cand_point.coor).sum(1)
     min_id = np.where(dist == np.min(dist))
-    return centers[min_id]
+    return min_id[0]
 
-def means_cluster(centers, point):
-    pass
+def K_means(filename, cluster_num):
+    global changes
+    # init
+    label = []
+    points_list = []
+    data_coor = np.loadtxt(filename, usecols=(0, 1, 2, 3), dtype=float, delimiter=',')
+    data_label = np.loadtxt(filename, usecols=(4,), dtype=str, delimiter=',')
 
-def K_means(x):
-    label = np.zeros(len(x))
-    features = x[:, 0:4]
-    new_centers = K_centers(x, center_num=5)
-    rest = features.remove(new_centers.centers)
-    ###
-    for point in rest:
-        centers = new_centers
-        new_centers, label = means_cluster(centers, point)
-    return label
+    # change str into float, for the convenience of computing
+    all_label = Counter(list(data_label)).keys()
+    for i in range(len(all_label)):
+        data_label[data_label == all_label[i]] = i
+    data_label = data_label.astype(float)
+    x = np.block([data_coor,data_label.reshape((-1,1))])
+
+    # init clusters
+    clusters = K_clusters(x, center_num=cluster_num)
+    clusters.initial()
+    for pt in x:
+        point = Point(pt[0:4], label=pt[4])
+        points_list.append(point)
+    init = 1
+
+    # do k-means
+    while changes > 5:
+        changes = 0
+        if init == 0:
+            clusters.restart()
+        for i, point in enumerate(points_list):
+            min_id = min_distance(clusters, point)
+            points_list[i] = clusters.centers[min_id[0]].get_in(point)
+        init = 0
+
+    # get label
+    for center in clusters.centers:
+        label.append(center.get_label())
+    for point in points_list:
+        point.label = label[point.id]
+
+    # compute the precision
+    acc = 0.
+    for point in points_list:
+        for i in range(len(data_coor)):
+            if cmp(list(data_coor[i]),list(point.coor)) == 0:
+                j = i
+                break
+        if data_label[j] == point.label:
+            acc += 1.
+    print "precision = ", acc/len(data_coor)
+
+    # print the result
+    for point in points_list:
+        print point.coor, all_label[int(point.label)], point.id
+
+K_means(filename, cluster_num=10)
